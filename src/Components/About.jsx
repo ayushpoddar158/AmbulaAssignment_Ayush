b9dc740b0252b239ee63be89952c0eda96b30f87
Ayush Poddar <ayushpoddar158@gmail.com>
2023-08-30 11:51:16 +0530

    initial commit

---
+++ src/Components/About.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../Styles/About.css'
+const About = () => {
+  return (
+    <>
+        <div className="mainAbout">
+     <div className="innerabout">
+     <h3>About us</h3>
+           <div> <p>
+               
+<h4 className='abouth4'><span>Welcome to Our Website:</span> Your Ultimate Productivity and Convenience Hub!</h4>
+
+At our website, we've curated a seamless and user-friendly online experience that combines three essential aspects of your daily life: task management, shopping, and staying informed about the weather. Our goal is to simplify your routine, helping you stay organized, shop with ease, and plan your activities based on real-time weather updates.
+            </p></div>
+     </div>
+        </div>
+    </>
+  )
+}
+
+export default About
---
+++ src/Components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../Styles/Home.css'
+const Home = () => {
+  return (
+    <>
+        <div className="mainHome">
+       <div className="innerHome">
+       <h2>Welcome to Ambula</h2>
+            <p>Life is a beautiful blend of tasks, shopping adventures, and weather surprises. Embrace the perfect synergy of these essential elements with our website, your ultimate destination for staying organized, shopping conveniently, and staying ahead of the weather curve.</p>
+       </div>
+        </div>
+    </>
+  )
+}
+
+export default Home
